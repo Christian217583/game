58eec4280d82e7cad081faa2fd68429708f91332
Christian217583 <113603431+Christian217583@users.noreply.github.com>
2023-02-14 23:10:24 -0500

    Add files via upload

---
+++ pygame/click_game.py
@@ -0,0 +1,25 @@
+character = Actor('cat1')
+character.topright = 0, 10
+
+WITDTH = 500
+HEIGHT = character.height + 96
+
+def draw():
+    screen.clear()
+    character.draw()
+
+def update():
+    character.left = character.left + 2
+    if character.left > WIDTH:
+        character.right = 0
+
+def on_mouse_down(pos):
+    if character.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("You missed me!")
+
+
+
+
+
